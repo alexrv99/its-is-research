38842c0a01a503ce8820d8eeda836c34c91615d8
Andres Peña <luis14-14@hotmail.com>
2019-11-26 23:24:26 -0600

    add files

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -9,7 +9,7 @@ const IndexPage = () => (
     <nav>
       <NavbarLink text="Resultados" href="#resultados" />
       <NavbarLink text="Objetivos" href="#objetivos" />
-      <NavbarLink text="Artículo" href="#articulo" />
+      <a download href="../statics/files/articulo.pdf">Articulo</a>
       <NavbarLink text="Conclusiones" href="#conclusiones" />
     </nav>
     <h1>El desinterés de los alumnos del ITS por la Ingeniería de Software</h1>
